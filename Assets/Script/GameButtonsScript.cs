deb88cc76a05e63b1d01b9641f52b6bd38ce64e5
MeMike <antomike@mail.ru>
2016-05-15 00:21:50 +0300

    last

---
+++ Assets/Script/GameButtonsScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameButtonsScript : MonoBehaviour {
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+}
