26ec86939e0937bcb7a1dd4fbb39a15c31e447ab
MeMike <antomike@mail.ru>
2016-05-14 16:08:34 +0300

    new

---
+++ Assets/HumanLittering.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanLittering : MonoBehaviour {
+    GameObject human1;
+    public Vector3 pos;
+    Quaternion rot;
+	void ILitter () {
+        pos.x = Random.Range(-380f, 380f);
+
+    Instantiate(human1,pos,rot);
+    
+            }
+}
---
+++ Assets/Musordetector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Musordetector : MonoBehaviour {
+    GameObject musor;
+    void OnTriggerEnter2D(Collider2D mus)
+    {
+       if (mus.tag == "Musor") {
+        
+        Debug.Log("I DESTROY");
+            musor = mus.gameObject;
+            Invoke("Destroyer", 0.3f);
+
+            
+
+         }
+    }
+    void Destroyer() {
+        Destroy(musor);
+    }
+}
