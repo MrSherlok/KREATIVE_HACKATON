d986c63b202a526bde763965450f7b2a924297bd
MrSherlok <antomike@mail.ru>
2016-05-19 17:53:54 +0300

    min fps

--- Assets/Script/trash/LitterScript.cs
+++ Assets/Script/trash/LitterScript.cs
@@ -14,14 +14,14 @@ public class LitterScript : MonoBehaviour {
         if (HumanScript.m == 100)
         {
             rb.velocity += Random.Range(10f, strenghUp) * Vector2.up;
-			if ( HumanScript.n<=0) rb.velocity += Random.Range(5f, strenghSide) * Vector2.right;
-			else rb.velocity += Random.Range(5f, strenghSide) * Vector2.left;
+			if ( HumanScript.n<=0) rb.velocity += Random.Range(7f, strenghSide) * Vector2.right;
+			else rb.velocity += Random.Range(7f, strenghSide) * Vector2.left;
         } else {
-            if (HumanScript.m <= 0.6f) rb.velocity += Random.Range(10f, strenghUp) * Vector2.up;
+			if (HumanScript.m <= -0.5f) rb.velocity += Random.Range(10f, strenghUp) * Vector2.up;
             else rb.velocity += Random.Range(5f, strenghUp-5f) * Vector2.up;
         
-			if ((HumanScript.n <= -3) || (HumanScript.n>=3)) rb.velocity += Random.Range(5f, strenghSide-2f) * Vector2.right;
-        else rb.velocity += Random.Range(5f, strenghSide-2f) * Vector2.left;
+			if ((HumanScript.n <= -3) || (HumanScript.n>=3)) rb.velocity += Random.Range(7f, strenghSide-3f) * Vector2.right;
+        else rb.velocity += Random.Range(7f, strenghSide-2f) * Vector2.left;
 		}
     }
 
